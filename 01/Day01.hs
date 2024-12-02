6b64b5ef32877b3ff8b5c605100b871a488dfaa7
devcayed <53474113+devcayed@users.noreply.github.com>
2024-12-02 21:22:50 +0100

    Remove redundant parenthesis and fix right-drift of code

--- 01/Day01.hs
+++ 01/Day01.hs
@@ -6,20 +6,22 @@ import           Data.List           (sort)
 import           System.IO           (IOMode (ReadMode), hGetContents, withFile)
 
 partOne :: IO ()
-partOne = withFile "./Input.txt" ReadMode (hGetContents >=> ( print
-                                                            . sum
-                                                            . fmap abs
-                                                            . uncurry (zipWith (-))
-                                                            . bimap sort sort
-                                                            . unzip
-                                                            . fmap ((\ [x, y] -> (read x, read y)) . words)
-                                                            . lines))
+partOne = withFile "./Input.txt" ReadMode (hGetContents >=> 
+    print . sum 
+          . fmap abs 
+          . uncurry (zipWith (-)) 
+          . bimap sort sort 
+          . unzip 
+          . fmap ((\ [x, y] -> (read x, read y)) . words) 
+          . lines)
 
 partTwo :: IO ()
-partTwo = withFile "./Input.txt" ReadMode (hGetContents >=> ( print
-                                                            . sum
-                                                            . (\ (xs, ys) -> fmap (\ x -> (* x) . length . filter (== x) $ ys) xs)
-                                                            . unzip
-                                                            . fmap ((\ [x, y] -> (read x, read y)) . words)
-                                                            . lines ))
+partTwo = withFile "./Input.txt" ReadMode (hGetContents >=> 
+    print . sum 
+          . (\ (xs, ys) -> fmap (\ x -> (* x) 
+          . length 
+          . filter (== x) $ ys) xs) 
+          . unzip 
+          . fmap ((\ [x, y] -> (read x, read y)) . words) 
+          . lines)
 
