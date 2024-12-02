169d4971b387c12f21c533176cf6a8a6104ec402
devcayed <53474113+devcayed@users.noreply.github.com>
2024-12-02 18:24:27 +0100

    Day 01

---
+++ 01/Main.hs
@@ -0,0 +1,26 @@
+module Day01 where
+
+import           Control.Applicative ((<*>))
+import           Control.Monad       ((>=>))
+import           Data.Bifunctor      (bimap)
+import           Data.List           (sort)
+import           System.IO           (IOMode (ReadMode), hGetContents, withFile)
+
+partOne :: IO ()
+partOne = withFile "./Input.txt" ReadMode (hGetContents >=> ( print
+                                                            . sum
+                                                            . fmap abs
+                                                            . uncurry (zipWith (-))
+                                                            . bimap sort sort
+                                                            . unzip
+                                                            . fmap ((\ [x, y] -> (read x, read y)) . words)
+                                                            . lines))
+
+partTwo :: IO ()
+partTwo = withFile "./Input.txt" ReadMode (hGetContents >=> ( print
+                                                            . sum
+                                                            . (\ (xs, ys) -> fmap (\ x -> (* x) . length . filter (== x) $ ys) xs)
+                                                            . unzip
+                                                            . fmap ((\ [x, y] -> (read x, read y)) . words)
+                                                            . lines ))
+
