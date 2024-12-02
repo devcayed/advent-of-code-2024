c673346a277178084dab5dc1841c975815cfe63e
devcayed <53474113+devcayed@users.noreply.github.com>
2024-12-02 21:27:15 +0100

    Remove redundant parenthesis and fix right-drift of code

--- 01/Day01.hs
+++ 01/Day01.hs
@@ -1,27 +1,27 @@
 module Day01 where
 
-import           Control.Monad       ((>=>))
-import           Data.Bifunctor      (bimap)
-import           Data.List           (sort)
-import           System.IO           (IOMode (ReadMode), hGetContents, withFile)
+import           Control.Monad  ((>=>))
+import           Data.Bifunctor (bimap)
+import           Data.List      (sort)
+import           System.IO      (IOMode (ReadMode), hGetContents, withFile)
 
 partOne :: IO ()
-partOne = withFile "./Input.txt" ReadMode (hGetContents >=> 
-    print . sum 
-          . fmap abs 
-          . uncurry (zipWith (-)) 
-          . bimap sort sort 
-          . unzip 
-          . fmap ((\ [x, y] -> (read x, read y)) . words) 
+partOne = withFile "./Input.txt" ReadMode (hGetContents >=>
+    print . sum
+          . fmap abs
+          . uncurry (zipWith (-))
+          . bimap sort sort
+          . unzip
+          . fmap ((\ [x, y] -> (read x, read y)) . words)
           . lines)
 
 partTwo :: IO ()
-partTwo = withFile "./Input.txt" ReadMode (hGetContents >=> 
-    print . sum 
-          . (\ (xs, ys) -> fmap (\ x -> (* x) 
-          . length 
-          . filter (== x) $ ys) xs) 
-          . unzip 
-          . fmap ((\ [x, y] -> (read x, read y)) . words) 
+partTwo = withFile "./Input.txt" ReadMode (hGetContents >=>
+    print . sum
+          . (\ (xs, ys) -> fmap (\ x -> (* x)
+          . length
+          . filter (== x) $ ys) xs)
+          . unzip
+          . fmap ((\ [x, y] -> (read x, read y)) . words)
           . lines)
 
