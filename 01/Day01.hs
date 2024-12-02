041e215bce2ffa5ec803ccf1459acb3ba98f385b
devcayed <53474113+devcayed@users.noreply.github.com>
2024-12-02 18:31:32 +0100

    Remove unused imports (<*>)

--- 01/Day01.hs
+++ 01/Day01.hs
@@ -1,6 +1,5 @@
 module Day01 where
 
-import           Control.Applicative ((<*>))
 import           Control.Monad       ((>=>))
 import           Data.Bifunctor      (bimap)
 import           Data.List           (sort)
